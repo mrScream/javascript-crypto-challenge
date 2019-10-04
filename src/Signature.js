be232c839a607cfb56d040762712b7c1f097d4b3
Halil <yagciogluhalil@gmail.com>
2019-10-04 18:31:46 +0200

    update signature.js

--- src/Signature.js
+++ src/Signature.js
@@ -1,1 +1,16 @@
+const libsodium=require('libsodium-wrappers');
+let keypair=null;
+let loadLibsodium=async()=>await libsodium.ready;
+(async()=>{
+  await loadLibsodium();
+  keypair=libsodium.crypto_sign_keypair();
+})();
 
+module.exports.verifyingKey=async function verifyingKey(){
+  await loadLibsodium();
+  return keypair.publicKey;
+}
+module.exports.sign=async function sign(msg){
+  return libsodium.crypto_sign(msg,keypair.privateKey)
+
+}
